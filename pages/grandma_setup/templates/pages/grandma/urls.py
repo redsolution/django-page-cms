5add727fc684e2f831438ea452bc1c24191b4be3
Ivan Gromov <summer.is.gone@gmail.com>
2010-08-15 23:39:25 +0600

    Added frontpage setting, added tinymce support, fixed template bugs

--- pages/grandma_setup/templates/pages/grandma/urls.py
+++ pages/grandma_setup/templates/pages/grandma/urls.py
@@ -1,3 +1,8 @@
+# Django-page-cms urls
+
 urlpatterns += patterns('',
+    {% if pages_settings.render_frontpage %}
+    url(r'^$', 'pages.views.details', name='pages-root'),
+    {% endif %}
     (r'^pages', include('pages.urls')),
 )
