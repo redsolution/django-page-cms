df42b778aeb7b9bf6c621c44ac31e51bbd97cccf
Ivan Gromov <ivan.gromov@redsolution.ru>
2012-04-11 15:11:45 +0600

    Removed strange AJAX behaviour

--- pages/views.py
+++ pages/views.py
@@ -60,9 +60,6 @@ class Details(object):
 
         template_name = self.get_template(request, context)
 
-        if request.is_ajax():
-            template_name = "body_%s" % template_name
-
         self.extra_context(request, context)
 
         answer = self.delegate(request, context, delegation)
